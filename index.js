a0cd20999d669b8b080a6197d5d8a96b92181918
Stephanie Frost <froststep69@gmail.com>
2025-02-18 09:13:56 +0100

    remove test link

--- index.js
+++ index.js
@@ -8,11 +8,3 @@ fetch("/data/destinations.json")
             document.querySelector("#root").append(sectionElm)
     }) 
 
-/*     fetch("/data/destinations.json")
-    .then (response => response.json())
-    .then (data => {
-        let sectionElm = document.createElement("section")
-        sectionElm.innerHTML = data.destinations.map(destination => `<a href="index.html?id=1">heknek</a>`).join("")
-            document.querySelector("#root").append(sectionElm)
-    })
- */
