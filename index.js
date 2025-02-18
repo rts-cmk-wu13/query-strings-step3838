507bbd9c1bb5d2785c032f11b834d938b9ca7ece
Stephanie Frost <froststep69@gmail.com>
2025-02-18 13:18:36 +0100

    jeg giver op

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-fetch("/data/destinations.json")
-    .then (response => response.json())
-    .then (data => {
-        let sectionElm = document.createElement("section")
-        sectionElm.innerHTML = data.destinations.map(destination => `<a href="destination.html?id=${destination.id}">
-            ${destination.title}
-            </a>`).join("")
-            document.querySelector("#root").append(sectionElm)
-    }) 
+let favorites = readFromLocalStorage("favorites") || []
+console.log(favorites);
 
+
+fetch("/data/destinations.json")
+    .then(response => response.json())
+    .then(data => {
+        let sectionElm = document.createElement("section")
+
+        sectionElm.innerHTML = data.destinations.map(destination => {
+            return `
+            <article class="class">
+                <figure class="card_imgcontainer">
+                    <img style="view-transition-name: image-transition-${destination.id}" src="/img/${destination.imagie}">
+                </figure>
+                <button class="card__favoritebtn"> data-favid="${destination.id}">heart</button>
+                <a href="destination.html?id=${destination.id}">
+                    more
+                </a>
+            </article>
+            `
+        })
+    })
